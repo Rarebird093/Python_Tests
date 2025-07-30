0873fc3779a88d20a9a6e8237e9c6c2cdd080103
Rarebird093 <copyninja093@gmail.com>
2025-07-30 23:26:41 +0530

    assignment1/task1.py
    This file consist information about task 1 in assignment 1

---
+++ Task_1.py
@@ -0,0 +1,15 @@
+# Input from the User
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+
+#Mathematical Operations
+a = x + y
+s = x - y
+m = x * y
+d = x / y
+
+#Printing Outputs
+print(f"Addition: " ,a)
+print(f"Subtraction: " ,s)
+print(f"Multiplication: " ,m)
+print(f"Division: " ,d)
