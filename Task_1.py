81541967438bd2eec293b3669490e6193f64d13e
Rarebird093 <copyninja093@gmail.com>
2025-08-03 19:46:23 +0530

    Assignment_3/Task_1
    This file contains python code which gives factorial of any given number .

---
+++ Task_1.py
@@ -0,0 +1,11 @@
+from sys import setrecursionlimit
+
+n = int(input("Enter a number: "))
+
+def factorial(n):
+    if n < 2 :
+        return 1
+    else:
+        return n * factorial(n-1)
+
+print(f"Factorial of {n} is: {factorial(n)}")
